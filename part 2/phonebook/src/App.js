b3806476d8a61afa6efdd08193cb314103f0c024
syedzayn69 <sazn2001@gmail.comm>
2023-07-05 22:51:01 +0500

    last edit

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,39 +1,103 @@
 import { useState } from 'react'
 
+// COMPONENTS
+const Filter = ({filterFn}) => {
+  return(
+    <>
+      filter shown with <input onChange={filterFn}/>
+    </>
+  )
+}
+const PersonForm = ({onSubmitFn,newName,changeFnName,newNum,changeFnNum}) => {
+  return(
+  <form onSubmit={onSubmitFn}>
+    <div>
+      name: <input value = {newName} onChange={changeFnName} required/><br />
+      number: <input value = {newNum} onChange={changeFnNum} required/><br />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form >)
+}
+const Person = ({IsEmpty,displayList,filterListFn}) => {
+  return IsEmpty ? displayList() : filterListFn() 
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+  const [IsEmpty, setIsEmpty] = useState(true)
+  const [filteredWords, setfilterWords] = useState('')
+  let isFound = false
+
 
   const onSubmitFn = (e) => {
     e.preventDefault()
+    isFound = false
     const newObj = {
-      name : newName 
+      name : newName,
+      number : newNum
     }
-    setPersons(persons.concat(newObj))
-    setNewName('')
-  }
+
+    // PREVENT USER FORM ENTERING THE SAME NAME TWICE
+    {persons.map(elem => {
+      if(elem.name === newObj.name){
+        alert(`${newName} already added to phonebook!`)
+        isFound = true
+      }
+    })
+    if(isFound === true) return
+    }
+
+  setPersons(persons.concat(newObj))
+  setNewName('')
+  setNewNum('')
+}
+
+
+const changeFnName = (e) => {
+  setNewName(e.target.value)
+}
+const changeFnNum = (e) => {
+  setNewNum(e.target.value)
+}
+const filterFn = (e) => {
+  let field = e.target.value
+  setfilterWords(field)
+
+  if(field !== '') setIsEmpty(false)
+  else setIsEmpty(true)
+}
+
+const filterListFn = () => {
+  let regex = new RegExp(`${filteredWords}`,'gi')
   
-
-  const changeFn = (e) => {
-    // console.log(e.target.value)
-    setNewName(e.target.value)
-  }
-
+  return persons.map((elem,i) => {
+    if(elem.name.match(regex) !== null){
+      return(
+        <div key={i}>{elem.name} {elem.number}</div>
+      )
+    }
+  })
+}
+const displayList = () => {
   return (
-    <div>
+    persons.map((elem,i) => {
+      return (<div key = {i}>{elem.name} {elem.number}</div>)
+    })
+  )
+}
+
+return (
+  <div>
       <h2>Phonebook</h2>
-      <form onSubmit={onSubmitFn}>
-        <div>
-          name: <input value = {newName} onChange={changeFn}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form >
-      <h2>Numbers</h2>
-      {persons.map((elem,index) => <div key = {index}>{elem.name}</div>)}
+      <Filter  filterFn={filterFn}/>
+      <h3>add a new</h3>
+      <PersonForm  onSubmitFn={onSubmitFn} newName={newName} changeFnName={changeFnName} newNum={newNum} changeFnNum={changeFnNum}/>
+      <h3>Numbers</h3>
+      <Person IsEmpty={IsEmpty} displayList={displayList} filterListFn={filterListFn}/>
     </div>
   )
 }
